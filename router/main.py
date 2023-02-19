f4eb17e0e2d2e9b20890063f4736226e786a3824
Samuel C <samuel.chicha@live.fr>
2023-02-19 20:42:34 +0000

    build:  routing

--- router/main.py
+++ router/main.py
@@ -4,48 +4,74 @@ import logging
 import logging.config
 import json
 
-with open("logging_conf.json", "r") as logging_config_file:
-    logging.config.dictConfig(json.load(logging_config_file))
 
-router_logger = logging.getLogger('router')
-ca_logger = logging.getLogger('ca')
 
-interfaces = [
-    {
-        "network": "A",
-        "ip": "192.168.1.2"
-    },
-    {
-        "network": "B",
-        "ip": "192.168.2.2"
-    }
-]
-
-def routing(interface_ip_address: str):
-    router_logger.debug(f"Inside thread {threading.current_thread().name}")
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # En écoute sur le couple adresse ip, port
-    sock.bind((interface_ip_address, 53))
-    router_logger.info(f"Listening on {interface_ip_address} port {53}")
-    # Paramètre de listen: Nombre de connexions non acceptés autorisé à être en attente
-    sock.listen(5)
-    while True:
-        (client_sock, address) = sock.accept()
-        router_logger.info(f"Connection with {address} started")
-        received = client_sock.recv(2048).decode()
-        router_logger.info(f"Received from {address}: {received}")
-        
+def routing(sock_src: socket.socket, ip_address_src: str, ip_address_dest: str):
+    # router_logger.debug(f"src: {ip_address_src}, dest: {ip_address_dest}")
+    msg = sock_src.recv(2048).decode()
+    sock_src.close()
+    router_logger.info(f"Receiving from {ip_address_src}: {msg}")
+    sock_dest = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_dest.connect((ip_address_dest, 53))
+    router_logger.info(f"Connecting to {ip_address_dest}")
+    sock_dest.sendall(msg.encode())
+    router_logger.info(f"Sending to {ip_address_dest}: {msg}")
+    
 
 def main():
     router_logger.info("Router starting")
     ca_logger.info("Certificate authority Starting")
-    threads = []
-    for interface in interfaces:
-        router_logger.debug(f"interface ip {interface['ip']}")
-        thread = threading.Thread(target=routing, args=[interface['ip']], name=f"thread-{interface['network']}")
-        threads.append(thread)
-        thread.start()
-        
+    sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # En écoute sur le couple adresse ip, port
+    sock_server.bind(("0.0.0.0", 53))
+    router_logger.info("Router listening on 0.0.0.0 on port 53.")
+    # Paramètre de listen: Nombre de connexions non acceptés autorisé à être en attente
+    sock_server.listen(5)
+    while True:
+        (sock_src, address_src) = sock_server.accept()
+        router_logger.info(f"Accepting new connection from {address_src[0]}")
+        client_src = next(filter(lambda client: client['ip'] == address_src[0], accepted_clients), None)
+        if client_src is not None:
+            # router_logger.debug(f"{client_src['ip']} is authorized")
+            client_dest = next(filter(lambda client: client['ip'] != address_src[0], accepted_clients), None)
+            if client_dest is not None:
+                # router_logger.debug("Routing")
+                thread = threading.Thread(target=routing, args=[sock_src, client_src['ip'], client_dest['ip']])
+                thread.start()
+            else:
+                router_logger.error(f"Could not find the other client")
+        else:
+            router_logger.warning(f"Unauthorized client: {address_src}")
+
 
 if __name__ == "__main__":
+    with open("logging_conf.json", "r") as logging_config_file:
+        logging.config.dictConfig(json.load(logging_config_file))
+
+    router_logger = logging.getLogger('router')
+    ca_logger = logging.getLogger('ca')
+
+    interfaces = [
+        {
+            "network": "A",
+            "ip": "192.168.1.2"
+        },
+        {
+            "network": "B",
+            "ip": "192.168.2.2"
+        }
+    ]
+
+    accepted_clients = [
+        {
+            "network": "A",
+            "ip": "192.168.1.3"
+        },
+        {
+            "network": "B",
+            "ip": "192.168.2.3"
+        }
+    ]
     main()
+else:
+    raise RuntimeError("Not the target")
--- site/main.py
+++ site/main.py
@@ -5,16 +5,38 @@ import logging.config
 import sys
 import json
 
-with open("logging_conf.json", "r") as logging_config_file:
-    logging.config.dictConfig(json.load(logging_config_file))
-site_logger = logging.getLogger('site')
+def listen(ip_address: str):
+    sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # site_logger.debug(f"IP: {ip_address}")
+    sock_server.bind(("0.0.0.0", 53))
+    site_logger.info("Listening 0.0.0.0 on port 53")
+    sock_server.listen(5)
+    while True:
+        (sock_src, address_src) = sock_server.accept()
+        site_logger.info(f"Accepting new connection from {address_src[0]}")
+        msg = sock_src.recv(2048).decode()
+        site_logger.info(f"Received from {address_src[0]}: {msg}")
 
-def main(ip_address: str):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip_address, 53))
-    site_logger.info(f"Connected to {ip_address}")
-    sock.sendall(f"yikes".encode())
+def main(id_site: str, ip_address: str):
+    site_logger.info(f"Site {id_site} started")
+    thread = threading.Thread(target=listen, args=[ip_address])
+    thread.start()
+    if id_site == "A":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ip_address, 53))
+        sock.sendall("yikes".encode())
+    
 
 if __name__ == "__main__":
-    ip_address = sys.argv[1]
-    main(ip_address)
+    if len(sys.argv) != 3:
+        raise ValueError("Invalid number of arguments.")
+    if sys.argv[1] not in ["A", "B"]:
+        raise ValueError("Invalid site identifier.")
+    with open("logging_conf.json", "r") as logging_config_file:
+        logging.config.dictConfig(json.load(logging_config_file))
+    id_site = sys.argv[1]
+    ip_address = sys.argv[2]
+    site_logger = logging.getLogger(f"site{id_site}")
+    main(id_site, ip_address)
+else:
+    raise RuntimeError("Not the target")
